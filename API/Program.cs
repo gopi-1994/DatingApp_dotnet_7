840a6b210ea4d0da67bc3d1ccd19b2df66a3d5a6
gopi-1994 <gopinath.v2507@gmail.com>
2023-10-18 23:35:32 +0530

    sECTION 3- HTTPS CONFIG, API MAPPED TO FRONT END

--- API/Program.cs
+++ API/Program.cs
@@ -15,19 +15,21 @@ builder.Services.AddDbContext<DataContext>(opt => {
 // // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 // if (app.Environment.IsDevelopment())
 // {
 //     app.UseSwagger();
 //     app.UseSwaggerUI();
 // }
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 
